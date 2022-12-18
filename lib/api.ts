91e3aa018f344fc292bbb102493e8d423f51205f
Rahul Mondal <rahulmondar@gmail.com>
2022-12-18 20:03:27 +0530

    feat: setup axios

---
+++ lib/api.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosInstance, Method } from "axios";
+
+const accessToken = localStorage.getItem("accessToken") || "";
+const refreshToken = localStorage.getItem("refreshToken") || "";
+
+export const $axiosV1 = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+        "X-Refresh": refreshToken,
+    },
+});
+
+export const invokeApi = async ({
+    axiosInstance,
+    path,
+    method = "GET",
+    body = {},
+    headers = {},
+    baseURL,
+}: {
+    axiosInstance: AxiosInstance;
+    path: string;
+    method: Method;
+    body: object;
+    headers: object;
+    baseURL?: string;
+}) => {
+    try {
+        const resp = await axiosInstance({
+            baseURL,
+            url: path,
+            method,
+            data: JSON.stringify(body),
+            headers: { ...headers },
+            timeout: 1000 * 60 * 3,
+        });
+        return resp.data;
+    } catch (error) {
+        throw error;
+    }
+};
